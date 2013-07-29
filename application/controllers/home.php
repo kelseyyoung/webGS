8ef48fd85199df885fbe2ee66d942a05a9c6157d
kelseyyoung <kelseyyoung4@gmail.com>
2013-07-28 18:35:58 -0700

    Made student & instructor view the home page

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,61 +8,27 @@
       $this->load->model('student_model');
     }
 
-    public function index($page = 'index') {
-      //Home page view
-      if (! file_exists('application/views/home/'.$page.'.php')) {
-        show_404();
+    public function index() {
+      $user = $this->session->userdata('type');
+      if (!$type) {
+	redirect(site_url('unauthorized'));
       }
-      $user = $this->session->userdata("user_id");
-      if ($user) {
-        //Figure out whether they're an instructor or student
-        //Go to that page
-        if( $this->session->userdata["type"] == "instructor" ) {
-          //Redirect to instructor/view
-          redirect(site_url('instructors/view'));
-        } else {
-          //Redirect to student/view
-          redirect(site_url('students/view'));
-        }
+      if ($type == "student") {
+	//Student
+	$data['student'] = $this->student_model->get_students($this->session->userdata('user_id'));
+	$data['title'] = "Students";
+	$this->load->view('templates/header', $data);
+	$this->load->view('students/view', $data);
+	$this->load->view('templates/footer');
       } else {
-        //Not logged in, take them to home page
-        $data['title'] = "Home";
+	//Instructor
+	$data['instructor'] = $this->instructor_model->get_instructors($this->session->userdata('user_id'));
+	$data['title'] = "Instructors";
+	$data['classes'] = $this->class_model->get_classes_by_instructor($this->session->userdata('user_id'));
 
-        $this->load->helper('form');
-        $this->load->library('form_validation');
-      
-        $this->form_validation->set_rules('username', 'Username', 'required');
-        $this->form_validation->set_rules('password', 'Password', 'required');
-
-        if ($this->form_validation->run() === FALSE) {
-          //Invalid or get
-          $this->load->view('templates/header', $data);
-          $this->load->view('home/'.$page);
-          $this->load->view('templates/footer');
-        } else {
-          //Log user in based on input
-          //Redirect to correct page
-          $iquery = $this->instructor_model->validate_instructor();
-          if (empty($iquery)) {
-            //Not an instructor, try student
-            $squery = $this->student_model->validate_student();
-            if (empty($squery)) {
-              //Invalid user, show form errors
-              $data["errors"] = "That username or password is invalid";
-              $this->load->view('templates/header', $data);
-              $this->load->view('home/'.$page);
-              $this->load->view('templates/footer');
-            } else {
-              //Is student, set session and redirect
-              $this->session->set_userdata(array('user_id' => $squery["id"], 'type' => "student"));
-              redirect(site_url('students/view'));
-            }
-          } else {
-            //Is instructor, set session and redirect
-            $this->session->set_userdata(array('user_id' => $iquery["id"], 'type' => 'instructor'));
-            redirect(site_url('instructors/view'));
-          }
-        }
+	$this->load->view('templates/header', $data);
+	$this->load->view('instructors/view', $data);
+	$this->load->view('templates/footer');
       }
     }
 
